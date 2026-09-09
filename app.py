81b671a8b33e92c62ee8a05b714c9cc169c56907
ferranvalletfuertes-dotcom <ferranvalletfuertes@gmail.com>
2026-09-09 12:43:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 import streamlit as st
-from interfaz import cargar_css, renderizar_logo
-from motor import inicializar_estados
-from vistas import render_login, render_escaner
-import streamlit as st
 from motor import inicializar_estados
 from vistas import render_login, render_escaner
 
@@ -13,31 +9,4 @@ inicializar_estados()
 if st.session_state.usuario_id is None:
     render_login()
 else:
-    render_escaner()
-    
-st.set_page_config(page_title="REFLEX AI", layout="wide", initial_sidebar_state="collapsed")
-
-inicializar_estados()
-cargar_css(st.session_state.tema)
-
-# 1. BARRERA DE SEGURIDAD
-if not st.session_state.usuario_id:
-    renderizar_logo(centrado=True)
-    render_login()
-
-# 2. LA MATRIZ AISLADA (Un solo jugador)
-else:
-    nav1, nav2, nav3 = st.columns([8, 1, 1])
-    with nav1: renderizar_logo(centrado=False)
-    with nav2:
-        if st.button("Cerrar Sesión", use_container_width=True): 
-            st.session_state.usuario_id = None
-            st.rerun()
-    with nav3:
-        modo = st.toggle("🌙", value=(st.session_state.tema == "oscuro"), label_visibility="collapsed")
-        if modo and st.session_state.tema == "blanco": st.session_state.tema = "oscuro"; st.rerun()
-        elif not modo and st.session_state.tema == "oscuro": st.session_state.tema = "blanco"; st.rerun()
-    
-    st.markdown("<hr style='margin-top: 0; border-color: #333;'>", unsafe_allow_html=True)
-    
     render_escaner()
